c54d876a6378a76b4d3a37a23bb83697b45ad8e9
caikkk2017 <32133200+caikkk2017@users.noreply.github.com>
2017-10-11 15:06:59 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QFont>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setWindowTitle(QString("四则运算生成器"));
+    QFont font;
+    font.setPointSize(20);
+    font.setFamily(("simsun"));
+    w.setBaseSize(1800,1000);
+    w.setFont(font);
+    w.show();
+
+    return a.exec();
+}
